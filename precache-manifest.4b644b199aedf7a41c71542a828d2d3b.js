dacfff74a838eee3683a6d7dc49651e77a661096
Son Nguyen <son@spiderbox.design>
2019-09-18 18:12:42 +0700

    Updates

---
+++ precache-manifest.4b644b199aedf7a41c71542a828d2d3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a87607a87b1bf2ff82755d407af16b8b",
+    "url": "/good-sleep/index.html"
+  },
+  {
+    "revision": "7c62d3ff635ad3208001",
+    "url": "/good-sleep/static/css/main.6bdde0e3.chunk.css"
+  },
+  {
+    "revision": "a87227912264a8eed7c5",
+    "url": "/good-sleep/static/js/2.11243069.chunk.js"
+  },
+  {
+    "revision": "7c62d3ff635ad3208001",
+    "url": "/good-sleep/static/js/main.dec1d0f3.chunk.js"
+  },
+  {
+    "revision": "c0591a74ee34d89deb30",
+    "url": "/good-sleep/static/js/runtime~main.fc85c735.js"
+  }
+]);
